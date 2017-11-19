7c5dd4fd7eace8a56d56d1f22548d2e35e412e67
Sadelise <sharhio@hotmail.com>
2017-11-19 13:21:35 +0200

    muutettu kansiorakennetta travisia ajatellen

--- src/test/java/lukuvinkkikirjasto/domain/BookTest.java
+++ src/test/java/lukuvinkkikirjasto/domain/BookTest.java
@@ -1,12 +1,17 @@
 package lukuvinkkikirjasto.domain;
 
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
 
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty"})
 public class BookTest {
 
     public BookTest() {
